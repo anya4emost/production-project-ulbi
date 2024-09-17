78a913d6c618bff160f18409ff92158c65419030
Anna Mikhaylova <annamikhailova1406@yandex.ru>
2024-09-17 12:20:01 +0300

    first module done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider/lib/useTheme';
+import { classNames } from '../shared/lib/classNames/classNames';
+import { AboutPage } from '../pages/AboutPage';
+import { MainPage } from '../pages/MainPage';
 
 export default function App() {
 
@@ -17,8 +17,8 @@ export default function App() {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
